7037522f43b1f804c3c83bcbfa04cb2e296e8d7e
NetcO <126324199+Narco360@users.noreply.github.com>
2023-12-22 09:56:54 +0100

    Update clientUDP.c

--- clientUDP.c
+++ clientUDP.c
@@ -7,9 +7,11 @@
 
      Compile using : gcc clientUDP.c -o clientUDP
 *********************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
